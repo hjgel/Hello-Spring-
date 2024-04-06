8812f3106125ea3758f601551aac73754baeca59
hjgel <143315340+hjgel@users.noreply.github.com>
2024-04-06 23:16:23 +0900

    2024.04.06 in DestTop v.02

--- hello-spring/src/main/java/Hyunjun/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/Hyunjun/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package Hyunjun.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,11 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data", "hello!");
         return "hello";
+    }
+
+    @GetMapping(
+            public String helloMvc(@RequestParam("n"hello-mvc")ame") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
